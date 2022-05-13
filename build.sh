0f83378c4835a3c80fd85d1d71740cfffcabf0d6
abdelmonem55 <99140522+abdelmonem55@users.noreply.github.com>
2022-05-13 23:09:21 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ set -e
 cd "`dirname $0`"
 cargo build --target wasm32-unknown-unknown --release
 cp $TARGET/wasm32-unknown-unknown/release/status_message.wasm ./res/
-#wasm-opt -Oz --output ./res/status_message.wasm ./res/status_message.wasm
+#wasm-opt -Oz --output ./res/status_message.wasm ./res/status_message.wasm 
 
